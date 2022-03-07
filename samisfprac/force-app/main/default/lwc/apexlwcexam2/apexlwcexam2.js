e29db9bd8497a33f8b4f3dae3cd700e63661ae15
syed sami <ssamiulla@quotient.com>
2022-03-07 19:55:54 +0530

    new change

--- samisfprac/force-app/main/default/lwc/apexlwcexam2/apexlwcexam2.js
+++ samisfprac/force-app/main/default/lwc/apexlwcexam2/apexlwcexam2.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
+import getContacts from '@salesforce/apex/apexlwcexam2.getContacts';
 
-export default class Apexlwcexam2 extends LightningElement {}
+export default class Apexlwcexam2 extends LightningElement {
+    @api recordId;
+    @track columns =[
+        {label:'Name',fieldName:'Name',type:'text'},
+        {label:'Phone',fieldName:'Phone',type:'text'},
+        {label:'Industry',fieldName:'Industry',type:'text'}
+    ]
+    connectedCallback(){
+        getAccount()
+            .then(result=>{
+                this.accountRecords = result;
+                console.log(accountRecords);
+            })
+            .catch(error=>{
+                this.errors = error;
+            })
+    }    
+}
